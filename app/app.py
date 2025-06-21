8770bd906fbd61c730eaba385773247ca931a29e
Krithiga-Balakrishnan <krithigadb96@gmail.com>
2025-06-21 22:43:38 +0530

    Changes made

--- app/app.py
+++ app/app.py
@@ -4,3 +4,4 @@ from main import app  # Import the FastAPI app instance from your main.py
 
 # Hugging Face Spaces will automatically detect this file
 # and launch the `app` instance using Uvicorn under the hood.
+#changes
